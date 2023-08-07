861d3a707f5f7a146e1d05e93fc650daa81519ca
luispdm <17044119+luispdm@users.noreply.github.com>
2023-08-07 18:12:43 +0200

    quiz solved

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples: u32) -> u32 {
+    let cost = if apples > 40 { 1 } else { 2 };
+    apples * cost
+    // this will work too
+    /* if apples <= 40 {
+        apples * 2
+    } else {
+        apples
+    } */
+}
 
 // Don't modify this function!
 #[test]
