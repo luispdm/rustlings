29b72ebab81a2d3a9178215b9afabf5203035192
luispdm <17044119+luispdm@users.noreply.github.com>
2023-08-07 17:57:57 +0200

    if solved

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -5,13 +5,14 @@
 //
 // Execute `rustlings hint if2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn foo_if_fizz(fizzish: &str) -> &str {
+    // the match statement would be more appropriate in this situation
     if fizzish == "fizz" {
         "foo"
+    } else if fizzish == "fuzz" {
+        "bar"
     } else {
-        1
+        "baz"
     }
 }
 
