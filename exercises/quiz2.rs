51bf1ad6ba6d2325ea7f622dc2d66ba508b24bc4
luispdm <17044119+luispdm@users.noreply.github.com>
2023-08-26 20:16:49 +0200

    quiz2

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,8 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -32,11 +30,32 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            let mut copy = string.clone();
+            match command {
+                Command::Uppercase => copy = copy.to_uppercase(),
+                Command::Trim => copy = copy.trim().to_string(),
+                Command::Append(times) => copy = format!("{copy}{}", "bar".repeat(*times))
+                // alternative 1:
+                // copy.push_str(&"bar".repeat(*times))
+                // alternative 2 - without the "repeat" method:
+                // {
+                //    for _i in 1..=*times {
+                //      copy.push_str("bar");
+                //    }
+                // }
+            }
+            output.push(copy);
+            // alternative - pushing the string to the array inside each match arm:
+            // match command {
+            //     Command::Uppercase => output.push(string.to_uppercase()),
+            //     Command::Trim => output.push(string.trim().to_string()),
+            //     Command::Append(times) => output.push(format!("{string}{}", "bar".repeat(*times)))
+            // }
         }
         output
     }
@@ -45,7 +64,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
