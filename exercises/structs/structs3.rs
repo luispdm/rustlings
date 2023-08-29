d495454adfc20de44249168041f23fd3db5e5f97
luispdm <17044119+luispdm@users.noreply.github.com>
2023-08-29 23:05:53 +0200

    fix structs3 after sync

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -33,7 +33,7 @@ impl Package {
         self.sender_country != self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+    fn get_fees(&self, cents_per_gram: u32) -> u32 {
         self.weight_in_grams * cents_per_gram
     }
 }
