4ecdcefe5f85bad70c3f4cc28c33d611edaa63ed
luispdm <17044119+luispdm@users.noreply.github.com>
2023-09-01 17:08:54 +0200

    options 2 & 3

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
@@ -13,9 +11,16 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
-    match y {
+    match &y { // instead of moving the value out of y, we just borrow it
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
     y; // Fix without deleting this line.
+    
+    /*
+     * Another solution is: "match y { Some (ref p)... }"
+     * 
+     * From the docs (difference between & and ref) https://doc.rust-lang.org/std/keyword.ref.html#-vs-ref :
+     * "&Foo matches different objects than Foo does. Some(ref foo) matches the same objects as Some(foo)."
+     */
 }
