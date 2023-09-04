e36b6910dd4079a65e880ecc9f19789c7132069a
luispdm <17044119+luispdm@users.noreply.github.com>
2023-09-04 17:19:20 +0200

    generics

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -6,9 +6,12 @@
 // Execute `rustlings hint generics1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
+    let mut shopping_list: Vec<&str> = Vec::new();
+    /*
+     * the exercise wants us to specify the generic type for Vec,
+     * but we could have removed the type definition entirely because Rust
+     * is able to infer the type of Vec with the following instruction
+     */
     shopping_list.push("milk");
 }
